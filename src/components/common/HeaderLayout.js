674e8665e882f5d1d6162d518db8c8057aa3fab4
Mohammed Shamil T H <shamilth2@gmail.com>
2025-07-23 18:28:35 +0530

    header height reduced

--- src/components/common/HeaderLayout.js
+++ src/components/common/HeaderLayout.js
@@ -4,7 +4,7 @@ export default function HeaderLayout() {
 
   return (
 
-    <div className="relative w-full h-44 bg-gradient-to-r from-lime-400 via-green-500 to-green-600 overflow-hidden">
+    <div className="relative w-full h-24 bg-gradient-to-r from-lime-400 via-green-500 to-green-600 overflow-hidden">
       <img src="/image.png" alt="Header Background" className="w-full h-full " />
        </div>
 
